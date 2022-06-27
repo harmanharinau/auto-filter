1fcfab6efb1f2f4305bd2cdefd366c9c81fc5594
harmanharinau <107754598+harmanharinau@users.noreply.github.com>
2022-06-27 14:09:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -9,7 +9,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, CUSTOM_FILE_CAPTION
-from utils import get_size, is_subscribed, save_group_settings, temp
+from utils import get_size
 from database.connections_mdb import active_connection
 import re
 import json
